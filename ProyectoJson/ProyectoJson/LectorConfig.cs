e8f51228f48c0bc42d03a04429bdddf907dc0a09
Enzo Eros Calvino <enzo-eros@hotmail.com>
2023-10-09 16:39:49 +0200

    Json proyecto

---
+++ ProyectoJson/ProyectoJson/LectorConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoJson
+{
+    internal class LectorConfig
+    {
+        public string Contrincante { get; set; } = "Humano";
+        public string Posicionamiento { get; set; } = "Automático";
+        public string Version { get; set; } = "1.0";
+
+    }
+}
---
+++ ProyectoJson/ProyectoJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace ProyectoJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string configPath = "config.json";
+
+            LectorConfig config;
+
+            if (File.Exists(configPath))
+            {
+                // LEER CONTENIDO JSON
+                string jsonContent = File.ReadAllText(configPath);
+
+                // DESERIALIZAR JSON EN OBJETO LECTORCONFIG
+                config = JsonConvert.DeserializeObject<LectorConfig>(jsonContent);
+            }
+            else
+            {
+                config = new LectorConfig();
+            }
+
+            // Acceder a los valores de configuración
+            Console.WriteLine($"Contrincante: {config.Contrincante}");
+            Console.WriteLine($"Posicionamiento: {config.Posicionamiento}");
+            Console.WriteLine($"Versión: {config.Version}");
+        }
+    }
+}
